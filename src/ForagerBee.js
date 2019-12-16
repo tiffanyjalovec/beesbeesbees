79c860a2511a03a1d61f1871663bb691f0d07429
Tiffany Jalovec <carr.tiffany@gmail.com>
2019-12-16 13:58:45 -0700

    my bees :)

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,15 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
+  this.age = 10;
 };
 
+ForagerBee.prototype = Object.create(Bee.prototype);
+
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
+}
--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,9 @@
-var Grub = function() {
+var Grub = function(color, food) {
+  this.age = 0;
+  this.color = "pink";
+  this.food = "jelly";
 };
 
+Grub.prototype.eat = function() {
+
+}
